03c4030024cd819c361514a6145edbb000475608
MathEngine1990 <119562563+MathEngine1990@users.noreply.github.com>
2025-08-08 05:31:06 +0300

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ import IntroScene from './scenes/IntroScene.js';
 import MainMenuScene from './scenes/MainMenuScene.js';
 import GameScene from './scenes/GameScene.js';
 
+import vkBridge from '@vkontakte/vk-bridge';
+
 const config = {
     type: Phaser.AUTO,
     parent: 'game-container',
@@ -12,4 +14,13 @@ const config = {
     scene: [BootScene, IntroScene, MainMenuScene, GameScene]
 };
 
-new Phaser.Game(config);
+// Запрос инициализации VK Mini App
+vkBridge.send('VKWebAppInit')
+  .then(() => {
+    console.log('VK Bridge инициализирован');
+    // можно запускать Phaser
+    new Phaser.Game(config);
+  })
+  .catch((e) => {
+    console.error('Ошибка VKBridge:', e);
+  });
