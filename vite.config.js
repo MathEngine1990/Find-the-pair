14defd3522b37be215c197ebbeb75d6bf775b00a
MathEngine1990 <119562563+MathEngine1990@users.noreply.github.com>
2025-09-13 17:17:24 +0300

    modules big updating
    1

---
+++ vite.config.js
@@ -0,0 +1,28 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+import legacy from '@vitejs/plugin-legacy';
+
+export default defineConfig({
+  plugins: [
+    legacy({
+      targets: ['defaults', 'not IE 11']
+    })
+  ],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          phaser: ['phaser'],
+          vk: ['@vkontakte/vk-bridge']
+        }
+      }
+    }
+  },
+  server: {
+    port: 3000,
+    open: true
+  }
+});
