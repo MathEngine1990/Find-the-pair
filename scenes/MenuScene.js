9c67e0fe219d8c118fa14308546d3039ca4a8641
MathEngine1990 <119562563+MathEngine1990@users.noreply.github.com>
2025-10-27 09:45:28 +0300

    556

--- scenes/MenuScene.js
+++ scenes/MenuScene.js
@@ -411,7 +411,7 @@ const topSafeZone = safeArea.top + 10; // 10px отступ от notch
       'titleMedium'
     );
     greeting.setOrigin(0.5,0);
-    greeting.setColor('#012615');
+    greeting.setColor('#243540');
     this.levelButtons.push(greeting);
 
     currentY += this.textManager.getSize('statLabel') + 30;
