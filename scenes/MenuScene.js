e5a29470445bf9c49e9996f5f96d9a15153bc081
MathEngine1990 <119562563+MathEngine1990@users.noreply.github.com>
2025-12-27 23:32:45 +0300

    Update MenuScene.js

--- scenes/MenuScene.js
+++ scenes/MenuScene.js
@@ -1092,7 +1092,7 @@ let currentY = safeArea.top + 10; // веб как было
 let headerX  = W / 2;            // веб центр
 
 if (isMobile) {
-  currentY = mobileLayout.topStartY+4; // вверх
+  currentY = mobileLayout.topStartY-24; // вверх
   headerX  = mobileLayout.contentX;  // влево
 }
 
